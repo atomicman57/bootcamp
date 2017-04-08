99d1a206a288364413aefe27fd57c1333fc3ffe3
blessing philips <blessingadesina@gmail.com>
2017-04-08 12:05:15 +0100

    Get Prime tests

---
+++ prime-spec.js
@@ -0,0 +1,24 @@
+var prime = require("../getPrime.js");
+describe("generates prime numbers from 0 to n ", function (){  
+
+	describe("no prime number is obtainable", function() {
+
+      it("should return an empty array", function() {
+        expect(aritGeo([])).toEqual([]);
+      });
+   	
+   	it("should return an array",function(){ 
+      expect(aritGeo([-1])).toEqual([]); 
+         
+  it("should return an array",function(){ 
+      expect(aritGeo([10])).toEqual([2,3,5,7]); 
+
+   it("should return an array",function(){ 
+      expect(aritGeo([20])).toEqual([2,3,5,7,11,13,17,19]); 
+   });     
+
+   it("should return an array",function(){ 
+      expect(aritGeo([50])).toEqual([2,3,5,7,11,13,17,19,23,29,31,37,41,43,47]); 
+   });     
+
+}
