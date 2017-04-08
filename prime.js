22eb7e6c21e4689c95236681f295101ba0ee060c
blessing philips <blessingadesina@gmail.com>
2017-04-08 11:55:26 +0100

    The get prime function

---
+++ prime.js
@@ -0,0 +1,22 @@
+function getPrimes(n){
+	if (typeof(n) == "number"){
+		if (n < 2){
+          return [];
+        } else
+var numbers = [], primes = [], maxNumber = n;
+
+for(var i = 2;i<=maxNumber;i++){
+ numbers.push(i);   
+}
+
+while(numbers.length){
+    primes.push(numbers.shift());
+    numbers = numbers.filter(
+        function(i){return i%primes[primes.length-1]!=0}
+    );
+}
+return primes
+}
+}
+
+console.log(getPrimes(1));
