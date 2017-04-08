a617bb5ebe0d5edd530e62863eaee27088c1a15d
atomicman57 <blessingadesina@gmail.com>
2017-04-08 12:18:09 +0100

    Update aritgeotest.js

--- aritgeotest.js
+++ aritgeotest.js
@@ -1,10 +1,4 @@
-*QUESTION 2:*
-Create a function called *aritGeo*  that takes one argument, an Array. You are required to check if the array passed as an argument is arithmetic, geometric, neither arithmetic nor geometric in progression or if the array is empty.
 
-For arithmetic progressions, return Arithmetic
-For geometric progressions, return Geometric
-For neither of the above, return -1
-For an empty array, return 0
 (function(){
   'use strict';
 
